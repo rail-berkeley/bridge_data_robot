88e46c71eb1dfbc88bb7e56187ff839d4e1632e2
Grace Tang <grace.tang@berkeley.edu>
2023-10-23 15:55:00 -0700

    Implement vild object detection class

--- widowx_envs/widowx_envs/utils/object_detection/object_detector_ViLD.py
+++ widowx_envs/widowx_envs/utils/object_detection/object_detector_ViLD.py
@@ -1,4 +1,4 @@
-from object_detector import ObjectDetector
+from widowx_envs.utils.object_detection.object_detector import ObjectDetector
 from scripts.ViLD import ViLD
 
 
@@ -7,9 +7,13 @@ class ObjectDetectorViLD(ObjectDetector):
         super().__init__(env, save_dir, image_xyz=image_xyz, detector_type='ViLD')
 
         self.game_board_str = "square game board"
-        self.category_name_string = ';'.join(['black checkers piece', 'white checkers piece', self.game_board_str])
+        self.category_name_string = ';'.join(['black circle', 'white circle', self.game_board_str])
         self.category_names = ['background'] + [x.strip() for x in self.category_name_string.split(';')]
         self.v = ViLD() 
 
-    def get_centroids(self, image): # need to change
-        return self.v.get_centroids('test1.jpg', self.category_names, self.game_board_str)[1]
+    def get_centroids(self, image_path): # need to change
+        return self.v.get_centroids(image_path, self.category_names, self.game_board_str)[2]
+        # return self.i.centroids
+
+    def initialize_integrator(self, i):
+        self.i = i
